e00161ba0a1a4a6b91fc1f8356b682b3560bb83d
Mario Suarez <masuarez107@gmail.com>
2022-09-25 11:35:31 -0500

    starting client side, configuring react-bootstrap basic

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import {createRoot} from 'react-dom/client';
+import App from './App';
+// Import our custom CSS
+import '../scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+    <App />
+)
