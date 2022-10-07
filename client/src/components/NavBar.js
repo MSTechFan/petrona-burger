96f6058c3375184ad1c68e330806aaecd5de149e
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-10-07 17:25:58 -0500

    10/07/2022 programming shoppingchart

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navBar.css'
+import ShoppingCart from './ShoppingCart'
 
 
 const NavBar = () => {
@@ -8,7 +9,7 @@ const NavBar = () => {
         <nav className="fixed-top navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Petrona Burger</a>
-                <button type="button" className="btn btn-warning">Pide ya!</button>
+                <ShoppingCart/>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
