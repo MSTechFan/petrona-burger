d32b49caebb6aea527cbab444f7c4993af37d398
Mario Suarez <masuarez107@gmail.com>
2022-12-12 23:15:29 -0500

    message

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-/* mongoose.connect("mongodb+srv://MSTechFan:CityBarranquilla2022@cluster0.obgityr.mongodb.net/petronaburger?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://MSTechFan:CityBarranquilla2022@cluster0.obgityr.mongodb.net/petronaburger?retryWrites=true&w=majority", {
     useNewUrlParser: true, useUnifiedTopology: true
-}) */
+})
 
 app.get('/api/products', (req, res) => {
     res.json({
